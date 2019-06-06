145c8dc5af478024fe74a5dab8ab5e84d0f0c17f
Israel_Jimenez <41273721+Erisjimver@users.noreply.github.com>
2019-06-06 10:40:59 -0500

    Add files via upload

---
+++ trabajador.js
@@ -0,0 +1,12 @@
+addEventListener("message",mensaje_recibido,false);
+
+
+function mensaje_recibido(e){
+	
+	var respuesta="El texto escrito en el cuadro es: "+e.data;
+
+	postMessage(respuesta);
+
+}
+
+
